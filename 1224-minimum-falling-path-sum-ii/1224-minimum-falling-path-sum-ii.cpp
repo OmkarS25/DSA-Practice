ee292718064a95485f6f6eca645980ed4c17e21c
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-04-27 05:14:59 +0530

    Time: 26 ms (86.77%), Space: 18.1 MB (57.39%) - LeetHub

--- 1224-minimum-falling-path-sum-ii/1224-minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/1224-minimum-falling-path-sum-ii.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
-        int n = grid.size(), res = INT_MAX;
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-
-        for(int j = 0; j < n; ++j) 
-            dp[0][j] = grid[0][j];
+        int n=grid.size();
         
-        for(int i = 1; i < n; ++i) {
-            for(int j = 0; j < n; ++j) {
-                int temp = INT_MAX;
-                for(int k = 0; k < n; ++k) {
-                    if(k != j) 
-                        temp = min(temp, grid[i][j] + dp[i - 1][k]);
-                    dp[i][j] = temp;
-                }
+        for(int i=1; i<n; i++){
+            auto row=grid[i-1];
+            nth_element(row.begin(), row.begin()+1, row.end());
+            for(int j=0; j<n; j++){
+                if (grid[i-1][j]==row[0])
+                    grid[i][j]+=row[1];
+                else
+                    grid[i][j]+=row[0];
             }
         }
-        for(int j = 0; j < n; ++j)
-            res = min(res, dp[n - 1][j]);
-        return res;
+        
+        return *min_element(grid[n-1].begin(), grid[n-1].end());
     }
 };
 
