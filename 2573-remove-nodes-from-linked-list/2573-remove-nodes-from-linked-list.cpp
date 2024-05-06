fa340321fd02beed5a9d532aa8584fd888eb9068
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-05-06 20:18:50 +0530

    Time: 207 ms (99.53%), Space: 164.2 MB (75.74%) - LeetHub

--- 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
-        if(!head->next) return head;
-        stack<int> st;
-        ListNode *temp=head;
-        while(head){
-            while(!st.empty() && st.top()<temp->val){
-                st.pop();
-            }
-            st.push(head->val);
-            head = head->next;
-            delete temp;
-            temp = head;
-        }
-        ListNode* curr = NULL;
-        while(!st.empty()){
-            head = new ListNode(st.top(),curr);
-            st.pop();
-            curr = head;
-        }
+        ios_base::sync_with_stdio(0);
+        if(head==NULL)return head;
+        head->next = removeNodes(head->next);
+        if(head->next==NULL)return head;
+        if(head->next->val>head->val)return head->next;
         return head;
     }
 };
+
+// If you want to save memory.
+// if(!head->next) return head;
+// stack<int> st;
+// ListNode *temp=head;
+// while(head){
+//     while(!st.empty() && st.top()<temp->val){
+//         st.pop();
+//     }
+//     st.push(head->val);
+//     head = head->next;
+//     delete temp;
+//     temp = head;
+// }
+// ListNode* curr = NULL;
+// while(!st.empty()){
+//     head = new ListNode(st.top(),curr);
+//     st.pop();
+//     curr = head;
+// }
+// return head;
