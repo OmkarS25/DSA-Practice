f5a8e09ed9eb27827cec5a50e8743564e4fdead7
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-05-21 00:43:01 +0530

    Time: 19 ms (44.93%), Space: 33.9 MB (64.15%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int index = -1, n = nums.size();
-        for(int i=0; i<n; i++){
-            int left=0, right=0;
-            for(int j=0; j<n; j++){
-                if(j<i) left += nums[j];
-                else if(j>i) right += nums[j]; 
-            }
-            if(left == right){
-                index = i;
-                break;
-            }
+        int total_sum = 0, left_sum = 0;
+        for (int num : nums) total_sum += num;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (left_sum == total_sum - left_sum - nums[i]) return i;
+            left_sum += nums[i];
         }
-        return index;
+        return -1;
     }
+
+    // MY solution
+    // int pivotIndex(vector<int>& nums){
+    // int index = -1, n = nums.size();
+    // for(int i=0; i<n; i++){
+    //     int left=0, right=0;
+    //     for(int j=0; j<n; j++){
+    //         if(j<i) left += nums[j];
+    //         else if(j>i) right += nums[j]; 
+    //     }
+    //     if(left == right){
+    //         index = i;
+    //         break;
+    //     }
+    // }
+    // return index;
+    // }
+
+    // accumulate example
+    // int pivotIndex(vector<int>& nums) {
+    //    for(int i=0;i<nums.size();i++){
+    //     int a=accumulate(nums.begin(),nums.begin()+i,0);
+    //     int b=accumulate(nums.begin()+i+1,nums.end(),0);
+    //     if(a==b){
+    //         return i;
+    //     }
+    //    }
+    // return -1;
+    // }
 };
