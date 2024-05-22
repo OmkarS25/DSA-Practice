2f0e19f8a6b44dc572c721311657c7a05c79c48e
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-05-22 19:19:28 +0530

    Time: 4 ms (15.94%), Space: 7.9 MB (13.39%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,8 +1,25 @@
 class Solution {
 public:
-    int strStr(const string &haystack, const string &needle) {
-        size_t pos = haystack.find(needle);
-        if (pos != string::npos) return static_cast<int>(pos);
-        return -1; // If the needle is not found
+    int strStr(string haystack, string needle) {
+        if (haystack == needle)
+            return 0;
+        if (needle.length() == 1) {
+            for (int i = 0; i <= haystack.length() - 1; i++) {
+                if (haystack[i] == needle[0])
+                    return i;
+            }
+            return -1;
+        }
+        for (int i = 0, j = needle.length(); i < haystack.length() - 1;
+             i++, j + needle.length()) {
+            if (haystack.substr(i, j) == needle)
+                return i;
+        }
+        return -1;
     }
+    // int strStr(const string &haystack, const string &needle) {
+    //     size_t pos = haystack.find(needle);
+    //     if (pos != string::npos) return static_cast<int>(pos);
+    //     return -1; // If the needle is not found
+    // }
 };
