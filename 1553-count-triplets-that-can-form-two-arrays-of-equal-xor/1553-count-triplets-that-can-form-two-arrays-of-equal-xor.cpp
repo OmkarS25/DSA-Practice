1691cede91c34def59804054a2e9ddfe952374a9
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-05-30 17:43:44 +0530

    Time: 0 ms (100%), Space: 9.1 MB (67.04%) - LeetHub

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
     int countTriplets(vector<int>& arr) {
-        int count = 0, n = arr.size();
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                int a = 0;
-                for(int k=i; k<j; k++){
-                    a ^= arr[k];
-                }
-
-                int b = 0;
-                for(int k=j; k<n; k++){
-                    b ^= arr[k];
-                    if(a==b) count++;
+        int count = 0;
+        
+        for (int i = 0; i < arr.size(); i++) {
+            int val = arr[i];
+            
+            for (int k = i + 1; k < arr.size(); k++) {
+                val ^= arr[k];
+                
+                if (val == 0) {
+                    count += (k - i);
                 }
             }
         }
+        
         return count;
     }
 };
+
+
+// Loop in loop in loop approach
+// class Solution {
+// public:
+//     int countTriplets(vector<int>& arr) {
+//         int count = 0, n = arr.size();
+//         for(int i=0; i<n; i++){
+//             for(int j=i+1; j<n; j++){
+//                 int a = 0;
+//                 for(int k=i; k<j; k++){
+//                     a ^= arr[k];
+//                 }
+//                 int b = 0;
+//                 for(int k=j; k<n; k++){
+//                     b ^= arr[k];
+//                     if(a==b) count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
