de837f6ee7679a4a49c4bf524210bbf07b6fc6fa
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-05-31 21:20:56 +0530

    Time: 5 ms (79.18%), Space: 12.3 MB (64.77%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         // Got the xor of two numbers.
-        int xornum = 0;
+        // int xornum = 0;
+        long xornum = 0;
         for(int a: nums){
             xornum ^= a;
         }
         // Now getting LSB which is 1, which will be 
         // the differece between the two numbers.
-        int diff_bit = 1;
-        while(!(xornum & diff_bit)){
-            diff_bit = diff_bit << 1;
-        }
+        // int diff_bit = 1;
+        // while(!(xornum & diff_bit)){
+        //     diff_bit = diff_bit << 1;
+        // }
+        int diff_bit = (xornum & xornum-1)^xornum;
         // Calculating the two different numbers.
         int a = 0, b = 0;
         for(int n : nums){
