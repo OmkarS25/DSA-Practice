9cf965131bbec9ef28bae7152d4ee709d93e3543
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-06-03 13:04:41 +0530

    Time: 3 ms (100%), Space: 11.7 MB (97.58%) - LeetHub

--- 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int appendCharacters(string s, string t) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(NULL);
         int slen = s.length(), tlen = t.length(), j = 0;
-        for(int i=0; i<slen; i++){
-            if(s[i] == t[j]){
-                if(j<tlen) j++;
-                else break;
-            }
+        for(int i=0; i<slen && j<tlen; i++){
+            if(s[i] == t[j]) j++;
         }
         return (tlen - j);
     }
