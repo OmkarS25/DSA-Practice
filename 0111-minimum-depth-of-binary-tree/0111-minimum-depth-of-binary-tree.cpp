e05876c766197a21cda4038d56b6e20667bc91cd
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-06-21 01:58:03 +0530

    Time: 133 ms (98.85%), Space: 101.7 MB (98.73%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -9,18 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    int helper(TreeNode* root, int count){
-        if(!root) return INT_MAX;
-        count++;
-        if(!root->left && !root->right){
-            return count;
-        }
-        return min((helper(root->left, count)), (helper(root->right, count)));
-    }
     int minDepth(TreeNode* root) {
-       if(!root) return 0;
-       return helper(root, 0); 
+        if (root == nullptr)
+            return 0;
+
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+
+        if (right == 0 || left == 0)
+            return max (left, right) + 1;
+        root->left = nullptr;
+        root->right = nullptr;
+        return min(left, right) + 1;
     }
 };
+
+
+// MY Code
+// class Solution {
+// public:
+//     int helper(TreeNode* root, int count){
+//         if(!root) return INT_MAX;
+//         count++;
+//         if(!root->left && !root->right){
+//             return count;
+//         }
+//         return min((helper(root->left, count)), (helper(root->right, count)));
+//     }
+//     int minDepth(TreeNode* root) {
+//        if(!root) return 0;
+//        return helper(root, 0); 
+//     }
+// };
