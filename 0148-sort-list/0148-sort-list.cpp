5aae99ca9fece4a358a6b8fa1070940aeea3c28c
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-06-27 09:54:10 +0530

    Time: 2736 ms (5.02%), Space: 56 MB (84.68%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,43 +10,28 @@
  */
 class Solution {
 public:
-    // ListNode *mergeTwoLists(ListNode *list1, ListNode *list2){
-    //     if (!list1) return list2;
-    //     if (!list2) return list1;
-    //     ListNode *ptr1 = list1, *ptr2 = list2;
-    //     ListNode *head = new ListNode(0);
-    //     ListNode *curr = head;
-    //     while (ptr1 && ptr2){
-    //         if (ptr1->val < ptr2->val){
-    //             curr->next = ptr1;
-    //             ptr1 = ptr1->next;
-    //         }
-    //         else{
-    //             curr->next = ptr2;
-    //             ptr2 = ptr2->next;
-    //         }
-    //         curr = curr->next;
-    //     }
-    //     if (ptr1) curr->next = ptr1;
-    //     if (ptr2) curr->next = ptr2;
-    //     ListNode *mergedList = head->next;
-    //     delete head;                       
-    //     return mergedList;
-    // }
-
     ListNode* sortList(ListNode* head) {
-        ListNode *dummyHead = new ListNode(), *temp = head;
-        vector<int> list;
-        while(temp){
-            list.push_back(temp->val);
-            temp = temp->next;
+        if(head==NULL) return head;
+        sortList(head->next);
+        ListNode* temp=head;
+        while(temp->next && temp->val>temp->next->val){
+            swap(temp->val,temp->next->val);
+            temp=temp->next;
         }
-        sort(list.begin(), list.end());
-        temp = dummyHead;
-        for(int a : list){
-            temp->next = new ListNode(a);
-            temp = temp->next;
-        }
-        return dummyHead->next;
+        return head;
+
+        // ListNode *dummyHead = new ListNode(), *temp = head;
+        // vector<int> list;
+        // while(temp){
+        //     list.push_back(temp->val);
+        //     temp = temp->next;
+        // }
+        // sort(list.begin(), list.end());
+        // temp = dummyHead;
+        // for(int a : list){
+        //     temp->next = new ListNode(a);
+        //     temp = temp->next;
+        // }
+        // return dummyHead->next;
     }
 };
