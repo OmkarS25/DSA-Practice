f4e5a2d4c27a1f67aeacd18fdc9b4b99f43ca853
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-07-09 04:32:03 +0530

    Time: 2 ms (75.49%), Space: 7.2 MB (62.44%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
+    // Bottom-up DP with constant space
     int findTheWinner(int n, int k) {
-        vector<int> circle;
-        for(int i=1; i<=n; i++){
-            circle.push_back(i);
+        int res = 0;
+        for (int player_num = 2; player_num <= n; ++player_num) {
+            res = (res + k) % player_num;
         }
-        int curr_ind = 0;
-        while(circle.size() > 1){
-            int next_to_remove = (curr_ind + k-1) % circle.size();
-            circle.erase(circle.begin() + next_to_remove);
-            curr_ind = next_to_remove;
-        }
-        return circle[0];
+        return res + 1;
     }
+
+    // Recursion With Subproblems
+    // int recursion(int n, int k) {
+    //     if (n == 1) {
+    //         return 0;
+    //     }
+    //     return (recursion(n - 1, k) + k) % n;
+    // }
+    // int findTheWinner(int n, int k) {
+    //     return recursion(n, k) + 1;
+    // }
+
+    // Simulation
+    // int findTheWinner(int n, int k) {
+    //     vector<int> circle;
+    //     for(int i=1; i<=n; i++){
+    //         circle.push_back(i);
+    //     }
+    //     int curr_ind = 0;
+    //     while(circle.size() > 1){
+    //         int next_to_remove = (curr_ind + k-1) % circle.size();
+    //         circle.erase(circle.begin() + next_to_remove);
+    //         curr_ind = next_to_remove;
+    //     }
+    //     return circle[0];
+    // }
 };
