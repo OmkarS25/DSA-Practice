761a68d4601bc454af30793447223d65b302a128
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-07-09 19:55:35 +0530

    Time: 114 ms (99.71%), Space: 76.7 MB (35%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.cpp
+++ 1803-average-waiting-time/1803-average-waiting-time.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
+        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
         int time = 0;
-        unsigned long long int total_sum = 0;
+        // unsigned long long int total_sum = 0;
+        double total_sum = 0;
         for(int i=0; i<customers.size(); i++){
             if (time < customers[i][0]) time = customers[i][0];
             time += customers[i][1];
             total_sum += time - customers[i][0];
         }
-        double ans = static_cast<double>(total_sum) / customers.size();
-        return ans;
+        // double ans = static_cast<double>(total_sum) / customers.size();
+        // return ans;
+        return total_sum / customers.size();
     }
 };
