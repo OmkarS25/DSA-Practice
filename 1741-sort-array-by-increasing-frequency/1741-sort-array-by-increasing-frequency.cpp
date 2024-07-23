ab13e02d6f6e9e9e3da218253ae5f2220337b5f2
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-07-23 23:32:47 +0530

    Time: 7 ms (55.39%), Space: 15.1 MB (14.29%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
+    static bool customcomp(int a, int b, const unordered_map<int, int>& list){
+        if (list.at(a) != list.at(b)) {
+            return list.at(a) < list.at(b);
+        }
+        return a > b; 
+    }
     vector<int> frequencySort(vector<int>& nums) {
         unordered_map<int,int> list;
         for(int a: nums){
             list[a]++;
         }
-        // sort(nums.begin(), nums.end(), customcomp);
         sort(nums.begin(), nums.end(), [&list] (int a, int b){
+            // return customcomp(a, b, list);
             if(list[a] != list[b]){
                 return list[a] < list[b];
             }
