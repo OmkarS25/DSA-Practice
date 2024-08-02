6341a6b577fe5ff3b89dad2c590d94516098f029
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-08-02 10:52:58 +0530

    Time: 42 ms (99.8%), Space: 83.4 MB (45.38%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -17,3 +17,18 @@ public:
         return mswap;
     }
 };
+
+// Second code, the upper one is the optimized one.
+// class Solution {
+// public:
+//     static int  minSwaps(vector<int>& nums) {
+//         const int n=nums.size(), n1=count(nums.begin(), nums.end(), 1);
+//         int mswap=INT_MAX, cnt1=0;
+//         for (int l=0, r=0; r < n+n1; r++) {
+//             cnt1+= nums[r%n];
+//             if (r-l+1 > n1) cnt1 -= nums[l++ % n];
+//             if (r-l+1 == n1) mswap = min(mswap, n1-cnt1);
+//         }
+//         return mswap;
+//     }
+// };
