b8bc4b5eed6abfd7db490c3cad9c1bc6b953b2bc
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-08-26 13:12:15 +0530

    Time: 8 ms (90.83%), Space: 15.5 MB (11.4%) - LeetHub

---
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,57 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+auto init = [](){ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+    vector<int> ans;
+    if (!root) return ans;
+    
+    stack<Node*> nodeStack;
+    stack<int> outputStack;
+    nodeStack.push(root);
+
+    while (!nodeStack.empty()) {
+        Node* node = nodeStack.top();
+        nodeStack.pop();
+        outputStack.push(node->val);
+
+        for (Node* child : node->children) {
+            if (child) {
+                nodeStack.push(child);
+            }
+        }
+    }
+
+    while (!outputStack.empty()) {
+        ans.push_back(outputStack.top());
+        outputStack.pop();
+    }
+
+    return ans;
+}
+};
