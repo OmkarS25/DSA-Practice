716fc591607e50c851a98f0bbf1973af70ffeee2
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-09-02 17:50:03 +0530

    Time: 42 ms (98.63%), Space: 77.1 MB (8.71%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -5,39 +5,16 @@ static const auto init = [](){
     return 'c';
 }();
 
-// class Solution {
-// public:
-//     int chalkReplacer(vector<int>& chalk, int k) {
-//         int i=0, n=chalk.size();
-//         unsigned long long int total = accumulate(chalk.begin(), chalk.end(), 0);
-//         k = k%total;
-//         while(k){
-//             if(k < chalk[i]) return i;
-//             k -=  chalk[i];
-//             ++i;
-//             i = i%n;
-//         }
-//         return i;  
-//     }
-// };
-
 class Solution {
 public:
-    int chalkReplacer(vector<int>& chalk, int initialChalkPieces) {
-        long long totalChalkNeeded = 0;
-        for (int studentChalkUse : chalk) {
-            totalChalkNeeded += studentChalkUse;
+    int chalkReplacer(vector<int>& chalk, int k) {
+        unsigned long long int total = 0;
+        for(int a : chalk) total += a;
+        k = k%total;
+        for(int i=0; i<chalk.size(); i++){
+            if(k < chalk[i]) return i;
+            k -=  chalk[i];
         }
-        
-        int remainingChalk = initialChalkPieces % totalChalkNeeded;
-        
-        for (int studentIndex = 0; studentIndex < chalk.size(); studentIndex++) {
-            if (remainingChalk < chalk[studentIndex]) {
-                return studentIndex;
-            }
-            remainingChalk -= chalk[studentIndex];
-        }
-        
-        return 0;
+        return 0;  
     }
 };
