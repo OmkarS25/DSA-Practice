297a97a4f24bb74af71c59019a4a39d6d87fa3c2
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-09-03 07:21:02 +0530

    Time: 0 ms (100%), Space: 10.3 MB (11.28%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,25 @@
+static const auto init = [](){ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        string number = "";
+        for (char x : s) {
+            number += to_string(x - 'a' + 1);
+        }
+        while (k > 0) {
+            int temp = 0;
+            for (char x : number) {
+                temp += x - '0'; 
+            }
+            number = to_string(temp);
+            k--;
+        }
+        return stoi(number);
+    }
+};
