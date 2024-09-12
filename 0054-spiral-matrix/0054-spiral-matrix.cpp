40a712ca3d6c7acca0bac09a2593bef0d753b48b
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-09-13 01:16:44 +0530

    Time: 0 ms (100%), Space: 8.5 MB (7.59%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -0,0 +1,50 @@
+#pragma GCC optimize("O3", "unroll-loops") 
+static const auto init = [](){ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if (matrix.empty()) return {};  // Edge case
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<int> ans(rows * cols);
+
+        int index = 0, i = 0, j = 0;
+        int rightBorder = cols - 1, leftBorder = 0;
+        int downBorder = rows - 1, upBorder = 0;
+
+        while (index < rows * cols) {
+            // Move right
+            while (j <= rightBorder && index < rows * cols) {
+                ans[index++] = matrix[i][j++];
+            }
+            j--; i++; upBorder++;
+
+            // Move down
+            while (i <= downBorder && index < rows * cols) {
+                ans[index++] = matrix[i++][j];
+            }
+            i--; j--; rightBorder--;
+
+            // Move left
+            while (j >= leftBorder && index < rows * cols) {
+                ans[index++] = matrix[i][j--];
+            }
+            j++; i--; downBorder--;
+
+            // Move up
+            while (i >= upBorder && index < rows * cols) {
+                ans[index++] = matrix[i--][j];
+            }
+            i++; j++; leftBorder++;
+        }
+
+        return ans;
+    }
+};
