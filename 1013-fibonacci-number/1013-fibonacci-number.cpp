b36707119b0a37bb108c06939a3f64b81b9ec0a8
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-09-13 01:34:26 +0530

    Time: 15 ms (16.93%), Space: 7.5 MB (34.48%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+#pragma GCC optimize("O3", "unroll-loops") 
+static const auto init = [](){ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        return fib(n-1) + fib(n-2);
+    }
+};
