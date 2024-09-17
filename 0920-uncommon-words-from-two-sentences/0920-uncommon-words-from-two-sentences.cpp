6b765f1c47cc1a7185dbd09e78df0297e318788a
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-09-17 15:03:24 +0530

    Time: 0 ms (100%), Space: 9 MB (45.08%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
@@ -1,24 +1,23 @@
+#pragma GCC optimize("O3", "unroll-loops") 
+static const auto init = [](){ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
-        string s = s1 + " " + s2;
-        cout<<s1.max_size();
-        unordered_map<string, int> map;
-        string temp = "";
-        for(char & val : s){
-            if(val == ' '){
-                map[temp]++;
-                temp = "";
-            }
-            else temp += val;
-        }
-        if(temp != ""){
-            map[temp]++;
-            temp = "";
+        string s = s1 + " " + s2, tmp;
+        stringstream str(s);
+        unordered_map<string,int> um;
+        while(str>>tmp){
+            um[tmp]++;
         }
         vector<string> ans;
-        for(auto& val : map){
-            if(val.second == 1) ans.emplace_back(val.first);
+        for(auto& val:um){ // auto it=um.begin();it<um.end();it++
+            if(val.second==1) ans.emplace_back(val.first); // can use push_back also
         }
         return ans;
     }
