1b133517eb75e9d9a7a941f912eddbee4884ab37
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-09-24 00:36:40 +0530

    Time: 92 ms (75.34%), Space: 85.5 MB (60.47%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -8,21 +8,43 @@ static const auto init = [](){
 
 class Solution {
 public:
-    unordered_map<string, int> um, memo;
-    int solve(string s) {
-        if (s.empty()) return 0;
-        if(memo.find(s) != memo.end()) return memo[s];
+    unordered_map<string, int> um;
+    unordered_map<int, int> memo;
+    int solve(string& s, int ind) {
+        if (ind >= s.size()) return 0;
+        if (memo.find(ind) != memo.end()) return memo[ind];  
         int extra = INT_MAX;
         string tmp = "";
-        for (int i = 0; i < s.size(); i++) {
+        for (int i = ind; i < s.size(); i++) {
             tmp += s[i];
-            if (um.find(tmp) != um.end()) extra = min(extra, solve(s.substr(i + 1)));
+            if (um.find(tmp) != um.end()) extra = min(extra, solve(s, i + 1));
         }
-        extra = min(extra, 1 + solve(s.substr(1)));
-        return  memo[s] = extra;
+        extra = min(extra, 1 + solve(s, ind + 1));
+        return memo[ind] = extra;
     }
     int minExtraChar(string s, vector<string>& dict) {
         for (auto& val : dict) um[val]++;
-        return solve(s);
+        return solve(s, 0);
     }
 };
+
+// class Solution {
+// public:
+//     unordered_map<string, int> um, memo;
+//     int solve(string s) {
+//         if (s.empty()) return 0;
+//         if(memo.find(s) != memo.end()) return memo[s];
+//         int extra = INT_MAX;
+//         string tmp = "";
+//         for (int i = 0; i < s.size(); i++) {
+//             tmp += s[i];
+//             if (um.find(tmp) != um.end()) extra = min(extra, solve(s.substr(i + 1)));
+//         }
+//         extra = min(extra, 1 + solve(s.substr(1)));
+//         return  memo[s] = extra;
+//     }
+//     int minExtraChar(string s, vector<string>& dict) {
+//         for (auto& val : dict) um[val]++;
+//         return solve(s);
+//     }
+// };
