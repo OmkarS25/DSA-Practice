9f9e1d214aaf914d822cc2602fce6db19f295f1d
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-09-28 07:58:06 +0530

    Time: 21 ms (48.11%), Space: 23.1 MB (11.65%) - LeetHub

--- 0859-design-circular-deque/0859-design-circular-deque.cpp
+++ 0859-design-circular-deque/0859-design-circular-deque.cpp
@@ -1,53 +1,114 @@
-#pragma GCC optimize("O3", "unroll-loops") 
-static const auto init = [](){ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
+#pragma GCC optimize ("Ofast")
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+#pragma GCC optimize ("-ffloat-store")
+#pragma GCC optimize ("O3", "unroll-loops")
+auto _=[]()noexcept{ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);return 0;}();
+
+class Node{
+public:
+    int data;
+    Node* prev;
+    Node* next;
+
+    Node(){
+        this->data = 0;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+
+
+    Node(int x){
+        this->data = x;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+
+};
 
 class MyCircularDeque {
-    int q[1000];
-    int k, front, back, len;
-
+    Node* left = new Node(-1);
+    Node* right = new Node(-1);
+    int size = 0, capacity = 0;
 public:
-    MyCircularDeque(int k) : k(k), front(0), back(k-1), len(0) {}
+    
+    void remove(Node* temp){
 
+        // Remove the node from between
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
+        temp->next = NULL;
+        temp->prev = NULL;
+
+
+    }
+
+    void add(Node* dummy, Node* temp){
+
+        dummy->prev->next = temp;
+        temp->prev = dummy->prev;
+        temp->next = dummy;
+        dummy->prev = temp;
+
+    }
+
+    MyCircularDeque(int k) {
+        left->next = right;
+        right->prev = left;
+        capacity = k;
+    }
+    
     bool insertFront(int value) {
-        if (len == k) return 0;
-        front = (front == 0) ? k - 1 : front - 1;
-        q[front] = value;
-        len++;
-        return 1;
-    }
+        
+        if(isFull()) return false;
 
+        Node* element = new Node(value);
+        add(left->next, element);
+        size++;
+        return true;
+    }
+    
     bool insertLast(int value) {
-        if (len == k) return 0;
-        back = (back == k - 1) ? 0 : back + 1; 
-        q[back] = value;                       
-        len++;
-        return 1;
-    }
+        if(isFull()) return false;
 
+        Node* element = new Node(value);
+        add(right, element);
+        size++;
+        return true;
+    }
+    
     bool deleteFront() {
-        if (len == 0) return 0;
-        front = (front == k - 1) ? 0 : front + 1;
-        len--;
-        return 1;
+        if(isEmpty()) return false;
+        
+        remove(left->next);
+        size--;
+        return true;
     }
-
+    
     bool deleteLast() {
-        if (len == 0) return 0;
-        back = (back == 0) ? k - 1 : back - 1;
-        len--;
-        return 1;
+        if(isEmpty()) return false;
+
+        remove(right->prev);
+        size--;
+        return true;
     }
+    
+    int getFront() {
+        if(isEmpty()) return -1;
 
-    int getFront() { return (len == 0) ? -1 : q[front]; }
+        return left->next->data;
+    }
+    
+    int getRear() {
+        if(isEmpty()) return -1;
 
-    int getRear() { return (len == 0) ? -1 : q[back]; }
-
-    bool isEmpty() { return len == 0; }
-
-    bool isFull() { return len == k; }
+        return right->prev->data;
+    }
+    
+    bool isEmpty() {
+        return size == 0;
+    }
+    
+    bool isFull() {
+        return size == capacity;
+    }
 };
