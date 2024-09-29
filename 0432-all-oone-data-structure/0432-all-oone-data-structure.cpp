a7cb0e457c73875eb0e7b67d691de7ea66e2b8f0
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-09-29 15:55:25 +0530

    Time: 2844 ms (5.09%), Space: 48.6 MB (99.77%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -1,3 +1,9 @@
+#pragma GCC optimize ("Ofast")
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+#pragma GCC optimize ("-ffloat-store")
+#pragma GCC optimize ("O3", "unroll-loops")
+static const auto _=[]()noexcept{ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);return 0;}();
+
 class AllOne {
     unordered_map<string, int> map;
 public:
