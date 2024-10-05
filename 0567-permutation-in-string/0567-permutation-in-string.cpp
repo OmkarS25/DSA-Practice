ca7bd8c8bd64e45d7c00e926e4aae35388849831
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-10-06 01:05:33 +0530

    Time: 7 ms (64.99%), Space: 8.9 MB (50.93%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -6,26 +6,45 @@ static const int _=[]()noexcept{ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);r
 
 class Solution {
 public:
-    bool helper(int index, const string & s2, unordered_map<char, int> map, int len) {
-        for (int i = index; i < index + len; i++) {
-            if (i >= s2.size() || map[s2[i]] == 0) return false;
-            map[s2[i]]--;  
-        }
-        return true;  
-    }
-
     bool checkInclusion(string s1, string s2) {
-        unordered_map<char, int> map;
-        bool ans = false;
-        int len = s1.size();
-        if(len > s2.size()) return false;
-        for(char ch: s1) map[ch]++;
-        for(int i = 0; i <= s2.size() - len; i++) {
-            if (map[s2[i]]) {
-                unordered_map<char, int> temp = map;
-                if (helper(i, s2, temp, len)) return true;  
-            }
+        if (s1.length() > s2.length()) return false;
+        vector<int> s1Count(26, 0), s2Count(26, 0);
+        for (int i = 0; i < s1.length(); ++i) {
+            s1Count[s1[i] - 'a']++;
+            s2Count[s2[i] - 'a']++;
         }
-        return false;
+        for (int i = 0; i < s2.length() - s1.length(); ++i) {
+            if (s1Count == s2Count) return true;
+            s2Count[s2[i] - 'a']--;
+            s2Count[s2[i + s1.length()] - 'a']++;
+        }
+        return s1Count == s2Count;
     }
 };
+
+// MY SOLUTION
+// class Solution {
+// public:
+//     bool helper(int index, const string & s2, unordered_map<char, int> map, int len) {
+//         for (int i = index; i < index + len; i++) {
+//             if (i >= s2.size() || map[s2[i]] == 0) return false;
+//             map[s2[i]]--;  
+//         }
+//         return true;  
+//     }
+
+//     bool checkInclusion(string s1, string s2) {
+//         unordered_map<char, int> map;
+//         bool ans = false;
+//         int len = s1.size();
+//         if(len > s2.size()) return false;
+//         for(char ch: s1) map[ch]++;
+//         for(int i = 0; i <= s2.size() - len; i++) {
+//             if (map[s2[i]]) {
+//                 unordered_map<char, int> temp = map;
+//                 if (helper(i, s2, temp, len)) return true;  
+//             }
+//         }
+//         return false;
+//     }
+// };
