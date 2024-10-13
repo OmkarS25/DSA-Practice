9d09978a118f2d1eab8d7d6f820dd7ddadea801c
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-10-13 23:23:01 +0530

    Time: 37 ms (87%), Space: 22.3 MB (55.25%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -5,39 +5,25 @@
 static const int _=[]()noexcept{ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);return 0;}();
 
 class Solution {
-private:
-    bool chk(int a,int b, int c, int d){
-        if(b-a < d-c) return true;
-        else if(b-a == d-c) if(a<c) return true;
-        return false;
-    }
 public:
-    vector<int> smallestRange(vector<vector<int>>& a) {
-        int ansx = -1e5, ansy = 1e5, total_list = a.size();
-        vector<pair<int,int>> vp;
-        for(int i=0;i<total_list;i++){
-            for(int j=0;j<a[i].size();j++){
-                vp.push_back({a[i][j],i});
-            }
-        }
-        sort(vp.begin(), vp.end());
-        unordered_map<int,int> um;
-        int l=0, r=0, n = vp.size();
-        while(r<n){
-            int num = vp[r].first, list = vp[r].second;
-            um[list]++;
-            while(um.size() >= total_list){
-                if(chk(vp[l].first,vp[r].first,   ansx, ansy )){
-                    ansx = vp[l].first;
-                    ansy = vp[r].first;
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        vector<pair<int, int>> ordered; 
+        for (size_t k = 0; k < nums.size(); ++k)
+            for (auto n: nums[k]) ordered.push_back({n, k});
+        sort(ordered.begin(), ordered.end());
+        int i = 0, k = 0;
+        vector<int> ans;
+        unordered_map<int, int> count;
+        for (size_t j = 0; j < ordered.size(); ++j) {
+            if (! count[ordered[j].second]++) ++k;
+            if (k == nums.size()) { 
+                while (count[ordered[i].second] > 1) --count[ordered[i++].second]; // minialize range
+                if (ans.empty() || ans[1] - ans[0] > ordered[j].first - ordered[i].first) {
+                    ans = vector<int>{ordered[i].first, ordered[j].first};
                 }
-                int old_list = vp[l].second;
-                um[old_list]--;
-                l++;
-                if(um[old_list]==0) um.erase(old_list);
             }
-            r++;
         }
-        return {ansx, ansy};
+
+        return ans;
     }
 };
