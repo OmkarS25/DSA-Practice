dbc4d3139e75b0d0ddaede4d799a8df4dd2b9f34
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-10-15 20:56:42 +0530

    Time: 19 ms (96.42%), Space: 15.7 MB (45.68%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     long long minimumSteps(string s) {
         long long res = 0;
-        int black = 0; 
-        for(int i=0; i<s.length(); i++){
-            if(s[i] == '0') res += (long long) black;
-            else black++;
-        }
+        for (int i = 0, j = 0; i < s.size(); ++i)
+            if (s[i] == '0') res += i - (j++);
         return res;
     }
 };
