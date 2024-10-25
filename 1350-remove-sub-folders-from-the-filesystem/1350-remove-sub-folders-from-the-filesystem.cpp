57da9f1927ebabba7ed86039dfb1c1d9c2d366ed
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-10-26 01:54:23 +0530

    Time: 46 ms (95.59%), Space: 47.9 MB (71.9%) - LeetHub

---
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(folder.begin(), folder.end());
+        vector<string> ans;
+        ans.push_back(folder[0]);
+        for(int i = 1; i < folder.size(); i++) {
+            string lastFolder = ans.back();
+            lastFolder.push_back('/');
+            if(folder[i].compare(0, lastFolder.size(), lastFolder) != 0) ans.push_back(folder[i]);
+        }
+        return ans;
+    }
+};
+
+#pragma GCC optimize ("Ofast")
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+#pragma GCC optimize ("-ffloat-store")
+#pragma GCC optimize ("O3", "unroll-loops")
+static const int _=[]()noexcept{ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);return 0;}();
