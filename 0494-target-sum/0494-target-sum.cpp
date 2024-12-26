7386fa62bd2ca0c1fc527d739646e020c108663f
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2024-12-26 21:09:28 +0530

    Time: 3 ms (88.31%), Space: 15.6 MB (40.79%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        unordered_map<string, int> memo;
-        function<int(int, int)> dfs = [&](int i, int sum) -> int {
-            if (i == nums.size()) return sum == target ? 1 : 0;
-            string key = to_string(i) + "," + to_string(sum);
-            if (memo.count(key)) return memo[key];
-            return memo[key] = dfs(i + 1, sum + nums[i]) + dfs(i + 1, sum - nums[i]);
-        };
-        return dfs(0, 0);
+        int n = nums.size(), sum = 0;
+        for(int i = 0;i<n; i++){
+            sum+=nums[i];
+        }  
+        if((sum+target)%2!=0) return 0;
+        if(abs(target)>abs(sum)) return 0;
+        int new_target = (sum+target)/2;
+        vector<vector<int>> t(n+1,vector<int>(new_target+1));
+        for(int i = 1; i<new_target+1; i++){
+            t[0][1] = 0;
+        }
+        t[0][0] = 1;
+        for(int i =1; i<n+1; i++){
+            for(int j = 0; j<new_target+1; j++){
+                if(nums[i-1]<=j) t[i][j] = t[i-1][j-nums[i-1]] + t[i-1][j];
+                else t[i][j] = t[i-1][j];
+            }
+        }
+        return t[n][new_target];
     }
 };
 
