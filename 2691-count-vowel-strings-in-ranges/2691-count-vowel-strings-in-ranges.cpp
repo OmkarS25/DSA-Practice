2de3384bd3afbd12f418417b56cdf3a1fdf067e3
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-01-03 05:11:42 +0530

    Time: 27 ms (13.79%), Space: 74.6 MB (20.07%) - LeetHub

---
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int isVowel(char ch){
+        string str = "aeiou";
+        return (str.find(ch) != string::npos);
+    }  
+
+    vector<int> vowelStrings(vector<string>& A, vector<vector<int>>& Q) {
+        int n = A.size();
+        vector<int> Pre(n,0);
+        for(int i=0;i<n;i++){
+            string temp = A[i];
+            if(isVowel(temp[0]) && isVowel(temp[temp.size()-1])){
+                Pre[i]++;
+            }
+            // cout << Pre[]
+        }
+        for(int i=1;i<n;i++){
+            Pre[i] += Pre[i-1];
+        }
+        vector<int> ans;
+        for(auto it : Q){
+            int l = it[0], r = it[1];
+            if(l==0) ans.push_back(Pre[r]);
+            else ans.push_back(Pre[r] - Pre[l-1]);
+        }
+        return ans;
+    }
+};
+
+#pragma GCC optimize ("O3", "unroll-loops")
+static const int _=[]()noexcept{
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    return 0;
+}();
