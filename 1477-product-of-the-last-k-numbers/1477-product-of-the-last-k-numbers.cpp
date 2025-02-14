06dcd4f927e28881c5afb660ab6350679a765150
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-02-15 03:57:27 +0530

    Time: 35 ms (34.39%), Space: 78.2 MB (27.38%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,13 +1,9 @@
 class ProductOfNumbers {
-private:
-    vector<int> v; // We will store the prefix product
-    volatile int n = 0;
+    vector<int> v {1}; // We will store the prefix product
+    int n = 0;
 
 public:
-    ProductOfNumbers() {
-        v.push_back(1);
-        n = 0;
-    }
+    ProductOfNumbers() {}
 
     void add(int num) {
         if (num == 0) {
