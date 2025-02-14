4b9fab1aabb69e39764f149ea128708df3f83ec4
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-02-15 03:52:46 +0530

    Time: 16 ms (81.75%), Space: 78.4 MB (18.65%) - LeetHub

---
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -0,0 +1,30 @@
+class ProductOfNumbers {
+private:
+    vector<int> v; // We will store the prefix product
+    int n = 0;
+
+public:
+    ProductOfNumbers() {
+        v.push_back(1);
+        n = 0;
+    }
+
+    void add(int num) {
+        if (num == 0) {
+            v = {1};
+            n = 0;
+        } 
+        else v.push_back(v[n++] * num);
+    }
+
+    int getProduct(int k) {
+        return (k > n) ? 0 : (v[n] / v[n - k]);
+    }
+};
+
+#pragma GCC optimize ("O3", "unroll-loops")
+static const int _=[]()noexcept{
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    return 0;
+}();
