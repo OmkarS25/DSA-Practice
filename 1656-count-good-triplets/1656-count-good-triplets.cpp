6bfb8a788bcb73684fc14603ace37d964c90d479
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-04-15 05:15:49 +0530

    Time: 8 ms (50.58%), Space: 11.5 MB (5.93%) - LeetHub

---
+++ 1656-count-good-triplets/1656-count-good-triplets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int count=0;
+        for(int i=0;i<arr.size();i++)
+        {
+            for(int j=i+1;j<arr.size();j++)
+            {
+                for(int k=j+1;k<arr.size();k++)
+                {
+                    if(abs(arr[i]-arr[j])<=a && abs(arr[j]-arr[k])<=b && abs(arr[i]-arr[k])<=c)
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+#pragma GCC optimize ("O3", "unroll-loops")
+static const int _=[]()noexcept{
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    return 0;
+}();
