1efe3e5c4a77087050db09c6e43ad0b2f5ff9844
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-04-19 00:42:12 +0530

    Time: 4 ms (56.36%), Space: 10 MB (53.06%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if (n == 0) return "";
+        string res = "1";
+        while (--n) {
+            string cur = "";
+            for (int i = 0; i < res.size(); i++) {
+                int count = 1;
+                while ((i + 1 < res.size()) && (res[i] == res[i + 1])) count++, i++;
+                cur += to_string(count) + res[i];
+            }
+            res = cur;
+        }
+        return res;
+    }
+};
+
+#pragma GCC optimize ("O3", "unroll-loops")
+static const int _=[]()noexcept{
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    return 0;
+}();
