b6b1e4f14fb84fbd4780f73d698c3089c91456a9
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-04-24 10:27:42 +0530

    Time: 1480 ms (17%), Space: 484.1 MB (8.97%) - LeetHub

---
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+        unordered_set<int> *distinctSet = new unordered_set<int>(nums.begin(), nums.end());
+        const int distinctCount = distinctSet->size(), len = nums.size();
+        delete distinctSet;
+        int count = 0;
+        for (int i = 0; i < len; i++) {
+            unordered_set<int> tempSet;
+            for (int j = i; j < len; j++) {
+                tempSet.insert(nums[j]);
+                if (tempSet.size() == distinctCount) {
+                    count += len - j;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+#pragma GCC optimize ("O3", "unroll-loops")
+static const int _=[]()noexcept{
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    return 0;
+}();
