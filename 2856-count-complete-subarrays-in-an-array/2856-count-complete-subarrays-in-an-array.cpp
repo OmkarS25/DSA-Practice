55cddb069884136c508c5915a358ccbd1fa33139
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-04-24 10:28:55 +0530

    Time: 1526 ms (12.18%), Space: 484 MB (8.97%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int countCompleteSubarrays(vector<int>& nums) {
-        unordered_set<int> *distinctSet = new unordered_set<int>(nums.begin(), nums.end());
-        const int distinctCount = distinctSet->size(), len = nums.size();
-        delete distinctSet;
+        unordered_set<int> distinctSet(nums.begin(), nums.end());
+        const int distinctCount = distinctSet.size(), len = nums.size();
         int count = 0;
         for (int i = 0; i < len; i++) {
             unordered_set<int> tempSet;
@@ -25,3 +24,25 @@ static const int _=[]()noexcept{
     cin.tie(0);cout.tie(0);
     return 0;
 }();
+
+// 1480 ms submit time
+// class Solution {
+// public:
+//     int countCompleteSubarrays(vector<int>& nums) {
+//         unordered_set<int> *distinctSet = new unordered_set<int>(nums.begin(), nums.end());
+//         const int distinctCount = distinctSet->size(), len = nums.size();
+//         delete distinctSet;
+//         int count = 0;
+//         for (int i = 0; i < len; i++) {
+//             unordered_set<int> tempSet;
+//             for (int j = i; j < len; j++) {
+//                 tempSet.insert(nums[j]);
+//                 if (tempSet.size() == distinctCount) {
+//                     count += len - j;
+//                     break;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
