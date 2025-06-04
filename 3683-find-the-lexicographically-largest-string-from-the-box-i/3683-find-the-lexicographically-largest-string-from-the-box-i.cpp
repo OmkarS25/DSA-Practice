4dcc87d6da5ac25b31a288d7f7bffe854fe53739
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-06-04 15:04:45 +0530

    Time: 71 ms (55.22%), Space: 169.3 MB (24.63%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -2,20 +2,11 @@ class Solution {
 public:
     string answerString(string word, int num) {
         if(num == 1) return word;
-        const int n=word.size();
-        string ans="";
+        int n = word.size(), len = n - num + 1;
+        string ans = "";
         
-        for(int i=0; i<n; i++){
-            string temp;
-            for(int j=i; j<n; j++){
-                int remleft = max(i, 0);
-                int remright = max(n-j-1, 0);
-                int cutspossible = remleft + remright;
-                
-                if(cutspossible >= (num-1)) temp.push_back(word[j]);
-                else break;
-            }
-            ans = max(ans, temp);
+        for(int i = 0; i < n; i++) {
+            ans = max(ans, word.substr(i, len));
         }
         return ans;
     }
