0f21a1a452ade1e9f8743a8907480e77b7e74f1d
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-06-27 23:24:32 +0530

    Time: 1254 ms (5.08%), Space: 475.5 MB (12.32%) - LeetHub

---
+++ 2140-longest-subsequence-repeated-k-times/2140-longest-subsequence-repeated-k-times.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string ans = "";
+    bool isSubsequence(string &s, string &temp) {
+        int i = 0;
+        for(auto &ch : s) {
+            if(i < temp.size() && ch == temp[i]) ++i;
+        }
+        return i == temp.size();
+    }
+    bool check(string &s, string &temp, int k) {
+        string repeated = "";
+        for(int i = 0; i < k; ++i) repeated += temp;
+        return isSubsequence(s, repeated);
+    }
+    void solve(string &s, string &temp, int k) {
+        for(auto ch = 'z'; ch >= 'a'; --ch) {
+            temp.push_back(ch);
+            if(check(s, temp, k)) {
+                // pick lexicographically greatest
+                if(temp.size() > ans.size() ||
+                (temp.size() == ans.size() && temp > ans)) ans = temp;
+
+                solve(s, temp, k);
+            }
+            temp.pop_back(); // backtrack
+        }
+    }
+    string longestSubsequenceRepeatedK(string s, int k) {
+        string temp = "";
+        solve(s, temp, k);
+        return ans;
+    }
+};
+
+#pragma GCC optimize ("O3", "unroll-loops")
+static const int _=[]()noexcept{
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    return 0;
+}();
