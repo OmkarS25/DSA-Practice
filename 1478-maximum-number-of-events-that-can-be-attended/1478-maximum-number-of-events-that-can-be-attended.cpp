61a48e93017d4036f596f8aefd6912584ce5a971
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-07-08 00:12:27 +0530

    Time: 56 ms (87.51%), Space: 74.4 MB (99.61%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
-        sort(events.begin(), events.end(), [](auto& a, auto& b){
-            return a[1] < b[1];  // sort by end day
-        });
+        const int n = events.size();
+        if(n == 1) return 1;
+        sort(events.begin(), events.end());
+        priority_queue<int, vector<int>, greater<int>> pq;
 
-        set<int> available;
-        // fill available days
-        for (int i = 1; i <= 100000; ++i) available.insert(i);
+        int res = 0, i = 0, day = 1;
 
-        int res = 0;
-        for (auto& event : events) {
-            int start = event[0], end = event[1];
-            auto it = available.lower_bound(start);
-            if (it != available.end() && *it <= end) {
-                available.erase(it);
+        // Find the last day among all events
+        int lastDay = 0;
+        for (auto& e : events) lastDay = max(lastDay, e[1]);
+
+        for (day = 1; day <= lastDay; ++day) {
+            // Add events starting today
+            while (i < n && events[i][0] == day) {
+                pq.push(events[i++][1]); // push end day
+            }
+
+            // Remove expired events
+            while (!pq.empty() && pq.top() < day) {
+                pq.pop();
+            }
+
+            // Attend one event today (pick the one that ends earliest)
+            if (!pq.empty()) {
+                pq.pop();
                 res++;
             }
         }
+
         return res;
     }
 };
