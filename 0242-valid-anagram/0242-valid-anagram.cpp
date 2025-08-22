0138295af469113872c9941a841d4b3353799ec7
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-08-22 11:13:10 +0530

    Time: 7 ms (20.54%), Space: 9.5 MB (98.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,25 +1,40 @@
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        int n = s.size(), m = t.size();
-        if(n != m) return 0;
-        unordered_map<char,int> mp1, mp2;
-        for(int i=0; i<n; i++){
-            mp1[s[i]]++;
-            mp2[t[i]]++;
-        }
-        std::unordered_map<char,int>::iterator it;
-        for(it = mp1.begin(); it!=mp1.end(); it++){
-            if(mp2.find(it->first) == mp2.end()) return 0;
-            if(it->second != mp2[it->first]) return 0;
-        }
-        return 1;
-    }
-};
-
 #pragma GCC optimize ("O3", "unroll-loops")
 static const int _=[]()noexcept{
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
     return 0;
 }();
+
+// Sorting Approach
+class Solution {
+public:
+    bool isAnagram(string s, string t){
+        int n = s.size(), m = t.size();
+        if(n != m) return 0;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for(int i=0; i<n; i++)
+            if(s[i] != t[i]) return 0;
+        return 1;
+    }
+};
+
+// HashMap Method
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         int n = s.size(), m = t.size();
+//         if(n != m) return 0;
+//         unordered_map<char,int> mp1, mp2;
+//         for(int i=0; i<n; i++){
+//             mp1[s[i]]++;
+//             mp2[t[i]]++;
+//         }
+//         std::unordered_map<char,int>::iterator it;
+//         for(it = mp1.begin(); it!=mp1.end(); it++){
+//             if(mp2.find(it->first) == mp2.end()) return 0;
+//             if(it->second != mp2[it->first]) return 0;
+//         }
+//         return 1;
+//     }
+// };
