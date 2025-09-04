7f35d4a476df2ac1f66cad405c546171899060dd
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-09-05 01:29:08 +0530

    Time: 0 ms (100%), Space: 8.5 MB (54.98%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.cpp
+++ 3830-find-closest-person/3830-find-closest-person.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
-    int findClosest(int x, int y, int z) {
-        x = abs(z-x), y = abs(z-y);
+    int findClosest(int& _x, int& _y, int& _z) {
+        int x = abs(_z-_x), y = abs(_z-_y);
         return x==y ? 0 : (x<y ? 1 : 2);
     }
 };
-
-#pragma GCC optimize ("O3", "unroll-loops")
-static const int _=[]()noexcept{
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    return 0;
-}();
