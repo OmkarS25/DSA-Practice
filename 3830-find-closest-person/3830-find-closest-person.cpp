5f99ea4e982cf303a1390d7920070126dddad76b
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-09-05 01:27:04 +0530

    Time: 0 ms (100%), Space: 8.7 MB (0.68%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.cpp
+++ 3830-find-closest-person/3830-find-closest-person.cpp
@@ -5,3 +5,10 @@ public:
         return x==y ? 0 : (x<y ? 1 : 2);
     }
 };
+
+#pragma GCC optimize ("O3", "unroll-loops")
+static const int _=[]()noexcept{
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    return 0;
+}();
