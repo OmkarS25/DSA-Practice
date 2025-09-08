a9927ae98f5400f0ded6c42ac975a3e1aadf657b
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-09-08 18:23:30 +0530

    Time: 74 ms (72.78%), Space: 36.8 MB (75.5%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,29 +1,38 @@
-#pragma GCC optimize ("Ofast")
-#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
-#pragma GCC optimize ("-ffloat-store")
-#pragma GCC optimize ("O3", "unroll-loops")
-static const int _=[]()noexcept{ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);return 0;}();
-
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        vector<pair<int, int>> ordered; 
-        for (size_t k = 0; k < nums.size(); ++k)
-            for (auto n: nums[k]) ordered.push_back({n, k});
-        sort(ordered.begin(), ordered.end());
-        int i = 0, k = 0;
-        vector<int> ans;
-        unordered_map<int, int> count;
-        for (size_t j = 0; j < ordered.size(); ++j) {
-            if (! count[ordered[j].second]++) ++k;
-            if (k == nums.size()) { 
-                while (count[ordered[i].second] > 1) --count[ordered[i++].second]; // minialize range
-                if (ans.empty() || ans[1] - ans[0] > ordered[j].first - ordered[i].first) {
-                    ans = vector<int>{ordered[i].first, ordered[j].first};
-                }
-            }
+        priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> pq;
+        int maxi = INT_MIN, mini = INT_MAX;
+
+        // process first k elements ko
+        for(int i=0; i<nums.size(); i++) {  
+            pq.push({nums[i][0], {i, 0}});
+            maxi = max(maxi, nums[i][0]);
+            mini = min(mini, nums[i][0]);
         }
 
-        return ans;
+        int ansStart = mini, ansEnd = maxi;
+
+        while(!pq.empty()) {
+            pair<int, pair<int,int>> topNode = pq.top();
+            int topData = topNode.first;
+            int topRow = topNode.second.first;
+            int topCol = topNode.second.second;
+            pq.pop();
+            mini = topData;
+            // compare krlete h for smaller range
+            if((maxi - mini) < (ansEnd - ansStart)) {
+                ansStart = mini;
+                ansEnd = maxi;
+            }
+            // insertion wala step
+            if(topCol + 1 < nums[topRow].size()) {
+                int newElement = nums[topRow][topCol + 1];
+                maxi = max(maxi, newElement);
+                pq.push({newElement, {topRow, topCol + 1}});
+            }
+            else break;
+        }
+        return {ansStart, ansEnd};
     }
 };
