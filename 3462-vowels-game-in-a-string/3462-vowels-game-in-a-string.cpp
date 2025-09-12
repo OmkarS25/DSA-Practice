392856008dda924087f9be97006f5c8e708a2e9f
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-09-12 13:58:52 +0530

    Time: 2 ms (89.18%), Space: 18.9 MB (79.48%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        for(char& ch : s)
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') return 1;
+        return 0;
+    }
+};
