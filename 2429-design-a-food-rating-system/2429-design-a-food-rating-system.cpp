c007af1123155bd349861972e723986f5ec6e1fb
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-09-17 20:24:00 +0530

    Time: 76 ms (97.23%), Space: 160.8 MB (93.35%) - LeetHub

--- 2429-design-a-food-rating-system/2429-design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/2429-design-a-food-rating-system.cpp
@@ -31,6 +31,13 @@ public:
     }
 };
 
+#pragma GCC optimize ("O3", "unroll-loops")
+static const int _=[]()noexcept{
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    return 0;
+}();
+
 // Used Hashmap and Ordered Set
 
 // class FoodRatings {
