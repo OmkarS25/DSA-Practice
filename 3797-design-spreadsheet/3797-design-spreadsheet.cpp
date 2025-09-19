c0b88019b0823e4faf035a4576c7f5d7ece54032
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-09-19 23:08:32 +0530

    Time: 91 ms (67.82%), Space: 191.6 MB (59.57%) - LeetHub

--- 3797-design-spreadsheet/3797-design-spreadsheet.cpp
+++ 3797-design-spreadsheet/3797-design-spreadsheet.cpp
@@ -36,3 +36,10 @@ public:
         return ans;
     }
 };
+
+#pragma GCC optimize ("O3", "unroll-loops")
+static const int _=[]()noexcept{
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    return 0;
+}();
