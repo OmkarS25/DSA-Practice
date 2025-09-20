3816d7f785f7e59b83792b224386576f8e0b3e0a
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-09-20 16:51:35 +0530

    Time: 272 ms (66.22%), Space: 429.2 MB (79.39%) - LeetHub

--- 3827-implement-router/3827-implement-router.cpp
+++ 3827-implement-router/3827-implement-router.cpp
@@ -55,3 +55,10 @@ public:
         return endIt - startIt;
     }
 };
+
+#pragma GCC optimize ("O3", "unroll-loops")
+static const int _=[]()noexcept{
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    return 0;
+}();
