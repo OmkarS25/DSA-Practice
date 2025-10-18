ed42e29c5d04e5808ab83887201d66d3106fd842
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-10-19 05:18:23 +0530

    Time: 114 ms (80.32%), Space: 97.8 MB (30.16%) - LeetHub

--- 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     int maxDistinctElements(vector<int>& nums, int k) {
+        const int n = nums.size();
         sort(nums.begin(), nums.end());
-        set<int> unique;
-        int lastUsed = INT_MIN;
-        for(int i=0; i<nums.size(); i++){
-            int rangeStart = max(lastUsed+1, nums[i]-k);
-            int rangeEnd = nums[i]+k;
-            if(rangeStart <= rangeEnd){
-                unique.insert(rangeStart);
-                lastUsed = rangeStart;
+        int cnt = 0, prev = INT_MIN, curr;
+        for (int i=0; i<n; i++) {
+            curr = min(max(nums[i] - k, prev + 1), nums[i] + k);
+            if (curr > prev) {
+                cnt++;
+                prev = curr;
             }
         }
-        return unique.size();
+        return cnt;
     }
 };
+
+// class Solution {
+// public:
+//     int maxDistinctElements(vector<int>& nums, int k) {
+//         sort(nums.begin(), nums.end());
+//         set<int> unique;
+//         int lastUsed = INT_MIN;
+//         for(int i=0; i<nums.size(); i++){
+//             int rangeStart = max(lastUsed+1, nums[i]-k);
+//             int rangeEnd = nums[i]+k;
+//             if(rangeStart <= rangeEnd){
+//                 unique.insert(rangeStart);
+//                 lastUsed = rangeStart;
+//             }
+//         }
+//         return unique.size();
+//     }
+// };
