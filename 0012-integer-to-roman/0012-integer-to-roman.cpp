115280dd1187d4e5e4aef9598cf4b73455bb3cf4
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-11-18 14:15:32 +0530

    Time: 0 ms (100%), Space: 12 MB (61.98%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,60 +1,16 @@
 class Solution {
 public:
-    string intToRoman(int num) {string res = "";
-        while(num){
-            if(num >= 1000){
-                res += "M";
-                num -= 1000;
-            }
-            else if(num >= 900){
-                res += "CM";
-                num -= 900;
-            }
-            else if(num >= 500){
-                res += "D";
-                num -= 500;
-            }
-            else if(num >= 400){
-                res += "CD";
-                num -= 400;
-            }
-            else if(num >= 100){
-                res += "C";
-                num -= 100;
-            }
-            else if(num >= 90){
-                res += "XC";
-                num -= 90;
-            }
-            else if(num >= 50){
-                res += "L";
-                num -= 50;
-            }
-            else if(num >= 40){
-                res += "XL";
-                num -= 40;
-            }
-            else if(num >= 10){
-                res += "X";
-                num -= 10;
-            }
-            else if(num >= 9){
-                res += "IX";
-                num -= 9;
-            }
-            else if(num >= 5){
-                res += "V";
-                num -= 5;
-            }
-            else if(num >= 4){
-                res += "IV";
-                num -= 4;
-            }
-            else if(num >= 1){
-                res += "I";
-                num -= 1;
+    string intToRoman(int num) {
+        vector<int> values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        string roman;
+        for(int i = 0; i < values.size() && num > 0; i++){
+            while(num >= values[i]){
+                num -= values[i];
+                roman += symbols[i];
             }
         }
-        return res;
+        return roman;
     }
 };
