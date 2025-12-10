6524d5b3a71ecbcbb2806511cdee300e4697c075
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-12-10 12:56:25 +0530

    Time: 0 ms (100%), Space: 92.9 MB (47%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int countPermutations(vector<int>& v) {
-        int n = v.size(), ans = 0, mod = 1e9+7;
-        sort(v.begin()+1, v.end());
-        if(v[0] >= v[1]) return 0;
-        ans++;
-        for(int i=1; i<n; i++){
-            ans = (1LL*ans*i)%mod;
+        const int n = v.size(), mod = 1e9+7;
+        long long ans = 1;
+        for (int i=1; i<n; i++) {
+            if (v[i] <= v[0]) return 0;
+            ans = (ans*i)%mod;
         }
         return ans;
     }
