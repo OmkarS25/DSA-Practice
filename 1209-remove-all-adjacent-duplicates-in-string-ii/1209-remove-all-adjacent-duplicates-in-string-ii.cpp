0dfcf8f186e3334ee7bba8762d34b408f2fe2cd8
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-12-10 21:41:15 +0530

    Time: 5 ms (66.47%), Space: 13.9 MB (66.76%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    bool areLastK_1CharsSame(string& ans, char& newch, int k_1){
-        int it = ans.size()-1;
-        for(int i=0; i<k_1; i++){
-            if(newch != ans[it]) return false;
-            it--;
-        }
-        return true;
-    }
-
     string removeDuplicates(string s, int k) {
-        string ans = "";
-        for(int i=0; i<s.size(); i++){
-            char& newch = s[i];
-            if(ans.size() < k-1) ans.push_back(newch);
-            else if(areLastK_1CharsSame(ans, newch, k-1))
-                for(int j=0; j<k-1; j++) ans.pop_back();
-            else ans.push_back(newch);
+        int i=0, j=0; 
+        int n = s.size();
+        vector<int> count(n, 0);
+        while(j<n){
+            s[i] = s[j];
+            count[i] = 1;
+            if(i>0 && s[i] == s[i-1]){
+                count[i] = count[i-1] + 1;
+                if(count[i] == k) i = i - k;
+            }
+            i++, j++;
         }
-        return ans;
+        return s.substr(0, i);
     }
 };
