31d91eed4f7870bfc50589a108a9ccf4ea8f68da
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-12-11 11:11:52 +0530

    Time: 29 ms (6.87%), Space: 60.7 MB (5.24%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    string addStringsUtil(string &num1, string &num2, int i, int j, int carry) {
-        // Base Case
-        if (i < 0 && j < 0 && carry == 0) return "";
+    void addRE(string& num1, int p1, string& num2, int p2, int carry, string& ans){
+        // base case
+        if(p1 < 0 && p2 < 0 && carry == 0) return;
 
-        // Ek Case Hum Solve Kar Rahe Hain
-        int n1 = (i >= 0) ? num1[i] - '0' : 0;
-        int n2 = (j >= 0) ? num2[j] - '0' : 0;
-        
-        int sum = n1 + n2 + carry;
-        carry = sum / 10;
-        char digit = (sum % 10) + '0';
-        // Recursive Call (Adding digits from right side to avoid reversing at the end)
-        string ans = addStringsUtil(num1, num2, i - 1, j - 1, carry) + digit;
-        return ans;
+        // ek case solve karunga
+        int n1 = (p1 >= 0 ? num1[p1] : '0') - '0';
+        int n2 = (p2 >= 0 ? num2[p2] : '0') - '0';
+        int csum = n1 + n2 + carry;
+        carry = csum / 10;
+        char digit = (csum % 10) + '0';
+        ans = digit + ans;
+        // recursive call
+        addRE(num1, p1 - 1, num2, p2 - 1, carry, ans);
     }
-    
     string addStrings(string num1, string num2) {
-        return addStringsUtil(num1, num2, num1.size() - 1, num2.size() - 1, 0);
+        string ans = "";
+        addRE(num1, num1.size() - 1, num2, num2.size() - 1, 0, ans);
+        return ans;
     }
 };
