5890bda00af9f0179682cb5a93b398ea0485d292
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2025-12-19 05:22:07 +0530

    Time: 24 ms (24.22%), Space: 328.3 MB (22.66%) - LeetHub

---
+++ 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
+        long long maxi =0;
+        const int n = prices.size();
+
+        vector<long long > prefix(n+1,0);
+        vector<long long > prefixProfit(n+1,0);
+
+        for(int i=0; i<n; i++){
+            maxi += prices[i]*strategy[i];
+            prefix[i+1] = prices[i];
+            prefix[i+1] += prefix[i];
+
+            prefixProfit[i+1] = prices[i]*strategy[i];
+            prefixProfit[i+1] += prefixProfit[i];
+        }
+
+        for(int i=k;i<=n;i++){
+            //[i-k+1,k] Window
+            long long ans =  prefixProfit[n] - (prefixProfit[i]-prefixProfit[i-k]);
+            ans += (prefix[i]-prefix[i-k/2]);
+            maxi = max(maxi,ans);
+        }
+
+        return maxi;
+    }
+};
