4d9509b21b868ddfcf6e6e57f6361fbffebe54a2
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2026-01-21 22:38:01 +0530

    Time: 0 ms (100%), Space: 28.1 MB (95.15%) - LeetHub

--- 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
@@ -3,10 +3,8 @@ public:
     vector<int> minBitwiseArray(vector<int>& nums) {
         vector<int> ans;
         ans.reserve(nums.size());
-        for (int n : nums) {
-            if (n % 2 == 0) ans.push_back(-1);
-            else ans.push_back(n - ((n+1 & -(n+1)) >> 1));
-        }
+        for (int n : nums)
+            ans.push_back((n % 2 == 0) ? -1 : (n - ((n+1 & -(n+1)) >> 1)));
         return ans;
     }
 };
