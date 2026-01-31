b0d90f6b0f4bbdeec08106cd96cabd0cab219924
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2026-02-01 00:10:22 +0530

    Time: 0 ms (100%), Space: 19.9 MB (24.67%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,9 +1,29 @@
+// Solution Using Binary Search
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        for(char ch : letters){
-            if(ch > target) return ch;
+        int beg = 0, end = letters.size() - 1;
+        char ans = letters[0];
+
+        while (beg <= end) {
+            int mid = beg + (end - beg) / 2;
+            if (letters[mid] > target) {
+                ans = letters[mid];
+                end = mid - 1;
+            } 
+            else beg = mid + 1;
         }
-        return letters[0];
+        return ans;
     }
 };
+
+// Solution Using For Loop
+// class Solution {
+// public:
+//     char nextGreatestLetter(vector<char>& letters, char target) {
+//         for(char ch : letters){
+//             if(ch > target) return ch;
+//         }
+//         return letters[0];
+//     }
+// };
