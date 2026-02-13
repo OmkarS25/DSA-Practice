8f1226e6a4a37be0581567a02f21a327333f1e85
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2026-02-14 01:29:30 +0530

    Time: 1016 ms (88.36%), Space: 346 MB (27.36%) - LeetHub

--- 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.cpp
+++ 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.cpp
@@ -1,70 +1,72 @@
 class Solution {
 public:
-    int longestBalanced(string s) {
-        const int n = s.length();
-        if (n <= 2) return n;   
-        
-        int ans = 1;
+    // For hashing pair in map 
+    struct pair_hash{
+        size_t operator()(const pair<int,int>&p) const{
+            return hash<int>()(p.first)^(hash<int>()(p.second)<<1);
+        }
+    };
 
-        // 1. Longest single character streak
-        char prev = s[0];
-        int count = 1;
-        for (int i = 1; i < n; i++) {
-            if (s[i] == prev) {
-                count++;
-                ans = max(ans, count);
-            } else {
-                count = 1;
-                prev = s[i];
+    // Two distinct
+    int twodistinct(const string& s, char c1, char c2){
+        map<int, int> pos;
+        pos[0] = -1;
+        int ans = 0;
+        int delta = 0;
+        for(int i = 0; i < (int)s.size(); i ++){
+            if(s[i] != c1 && s[i] != c2){
+                pos.clear();
+                pos[0] = i;
+                delta = 0;
+                continue;
             }
-        }
-
-        // 2. Two-character case
-        vector<pair<char, char>> pairs = {{'a', 'b'}, {'b', 'c'}, {'a', 'c'}};
-
-        for (auto p : pairs) {
-            char c1 = p.first;
-            char c2 = p.second;
-            char other = 'a' + 'b' + 'c' - c1 - c2;
-
-            unordered_map<int, int> mp;
-            mp[0] = -1;
-            int diff = 0;
-
-            for (int i = 0; i < n; i++) {
-                if (s[i] == other) {
-                    mp.clear();
-                    mp[0] = i;
-                    diff = 0;
-                } else {
-                    if (s[i] == c1) diff++;
-                    else diff--;
-
-                    if (mp.count(diff))  ans = max(ans, i - mp[diff]);
-                    else mp[diff] = i;
-                }
+            if(s[i] == c1)
+                delta++;
+            else
+                delta--;
+            if(pos.find(delta) != pos.end()){
+                ans = max(ans, i - pos[delta]);
             }
+            else
+                pos[delta] = i;
         }
-
-        // 3. Three-character case
-        vector<int> f(3, 0);
-        map<vector<int>, int> mp3;
-        mp3[{0, 0, 0}] = -1;
-
-        for (int i = 0; i < n; i++) {
-            f[s[i] - 'a']++;
-
-            int mn = min({f[0], f[1], f[2]});
-            vector<int> key = {
-                f[0] - mn,
-                f[1] - mn,
-                f[2] - mn
-            };
-
-            if (mp3.count(key)) ans = max(ans, i - mp3[key]);
-            else mp3[key] = i;
-        }
-
         return ans;
+    }
+
+    // Main function
+    int longestBalanced(string s) {
+        int mx=1,i,j=0,n=s.size();
+
+        // Single char
+        for(i=0;i<n-1;i++){
+            char ch=s[i];
+            j=i;
+            while(i<n&&s[i]==ch)
+                i++;
+            mx=max(i-j,mx);
+            i--;
+        }
+
+        // Two distinct
+        int zz=max(twodistinct(s,'a','b'),twodistinct(s,'b','c'));
+        zz=max(zz,twodistinct(s,'a','c'));
+        mx=max(zz,mx);
+
+        // All three char
+        unordered_map<pair<int,int>,int,pair_hash>mp;
+        int cnb=0,cna=0,cnc=0;
+        mp[{0,0}]=-1;
+        for(i=0;i<n;i++){
+            if(s[i]=='a') cna++;
+            else if(s[i]=='b') cnb++;
+            else cnc++;
+
+            if(mp.find({cnb-cna,cnc-cna})!=mp.end()){
+                mx=max(mx,i-mp[{cnb-cna,cnc-cna}]);
+            }
+            else mp[{cnb-cna,cnc-cna}]=i;
+        }
+
+        return mx;
     }
 };
