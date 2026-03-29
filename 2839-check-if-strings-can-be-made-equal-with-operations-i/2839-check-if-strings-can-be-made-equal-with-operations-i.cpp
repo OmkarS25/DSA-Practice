27ac22df30eb5367ff5ef849b176a9a683edb3be
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2026-03-29 12:22:59 +0530

    Time: 0 ms (100%), Space: 8.3 MB (49.03%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        if(s1 == s2) return true;
-        string temp = "";
-        temp = temp + s1[2] + s1[1] + s1[0] + s1[3];
-        if (temp == s2) return true;
-        temp = "";
-        temp = temp + s1[0] + s1[3] + s1[2] + s1[1];
-        if (temp == s2) return true;
-        temp = "";
-        temp = temp + s1[2] + s1[3] + s1[0] + s1[1];
-        if (temp == s2) return true;
-        return false;
+        for(int i = 0; i < 2; i++){
+            if(s1[i] != s2[i]){
+                swap(s1[i], s1[i+2]);
+            }
+        }
+        return s1 == s2;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool canBeEqual(string s1, string s2) {
+//         if(s1 == s2) return true;
+//         string temp = "";
+//         temp = temp + s1[2] + s1[1] + s1[0] + s1[3];
+//         if (temp == s2) return true;
+//         temp = "";
+//         temp = temp + s1[0] + s1[3] + s1[2] + s1[1];
+//         if (temp == s2) return true;
+//         temp = "";
+//         temp = temp + s1[2] + s1[3] + s1[0] + s1[1];
+//         if (temp == s2) return true;
+//         return false;
+//     }
+// };
