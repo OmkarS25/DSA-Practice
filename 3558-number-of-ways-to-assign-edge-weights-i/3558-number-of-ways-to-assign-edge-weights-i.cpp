27c73b5a2989a197f8ba3fc7d6e05008cd10257f
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2026-06-12 05:14:39 +0530

    Time: 315 ms (41.45%), Space: 344.2 MB (37.5%) - LeetHub

---
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int MOD = 1e9 + 7;
+
+    int dfs(int node, vector<vector<int>>& adj, vector<bool>& visited){
+        visited[node] = true;
+        int maxDepth = 0;
+        for(auto& nei : adj[node]){ // traversal the node of the parent node
+            if(!visited[nei]){ // if not visited the child node
+                int depth = 1 + dfs(nei, adj, visited); // what every is full depth + 1
+                maxDepth = max(maxDepth, depth); // compare the depth of all nodes and return the maxdepth for this parent node.
+            }
+        }
+        return maxDepth;
+    }
+
+    int assignEdgeWeights(vector<vector<int>>& edges) {
+        const int n = edges.size() + 1; // Bcz the edges.size == n-1
+        vector<vector<int>> adj(n + 1);
+        vector<bool> visited(n +1, false);
+
+        for(auto& edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        int maxDepth = dfs(1, adj, visited);
+
+        vector<vector<long long>> dp(maxDepth+1, vector<long long>(2, 0));
+        dp[0][0] = 1; // Base Case
+
+        for(int i=1; i<=maxDepth; i++){
+            dp[i][0] = (dp[i-1][0] + dp[i-1][1]) % MOD;
+            dp[i][1] = (dp[i-1][0] + dp[i-1][1]) % MOD;
+        }
+        
+        return dp[maxDepth][1] % MOD;
+    }
+};
