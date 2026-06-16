1494986120cc5b16425bca50b20c00cfa9c74b6d
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2026-06-16 20:08:29 +0530

    Time: 3 ms (76.89%), Space: 40.3 MB (17.8%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -3,18 +3,14 @@ public:
     string processStr(string s) {
         string ans = "";
         for(int i=0; i<s.length(); i++){
-            if(s[i] == '*' && ans.size() > 0) {
-                ans = ans.substr(0, ans.length()-1);
+            if(s[i] == '*' || s[i] == '#' || s[i] == '%') {
+                if(ans.size()>0) {
+                    if(s[i] == '*') ans = ans.substr(0, ans.length()-1);
+                    else if(s[i] == '#') ans += ans;
+                    else if(s[i] == '%') reverse(ans.begin(), ans.end());
+                }
             }
-            else if(s[i] == '#' && ans.size() > 0) {
-                ans += ans;
-            }
-            else if(s[i] == '%' && ans.size() > 1) {
-                reverse(ans.begin(), ans.end());
-            }
-            else if(s[i] != '*' && s[i] != '#' && s[i] != '%') {
-                ans += s[i];
-            }
+            else ans += s[i];
         }
         return ans;
     }
