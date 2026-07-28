d844a815f6af8b5bb19d850578623c2f24c82e71
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2026-07-28 11:36:26 +0530

    Time: 22 ms (83.74%), Space: 73.7 MB (45.93%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int charmap[26] = {0};
+        string ans = "";
+        int odd = -1; // Odd char index
+        
+        // Mapping the chars
+        for(char ch : s){
+            charmap[ch-'a']++;
+        }
+
+        for(int i=0; i<26; i++){
+            if(charmap[i] != 0){
+                if(charmap[i]%2 != 0) odd = i;
+                int count = charmap[i]/2;
+                for(int j=0; j<count; j++){
+                    ans.push_back(i + 'a');
+                }
+            }
+        }
+        string other_half = ans;
+        reverse(other_half.begin(), other_half.end());
+        if(odd != -1) ans.push_back(odd + 'a');
+        ans += other_half;
+        return ans;
+    }
+};
