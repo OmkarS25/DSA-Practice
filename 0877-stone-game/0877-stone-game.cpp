5c056638e90910075b7c162cedce4a2bc4f528fd
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2026-08-02 06:04:18 +0530

    Time: 0 ms (100%), Space: 10.5 MB (68.24%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,17 +1,43 @@
+// Solution -2 : Bottom-Up DP | Space- Optimized
+
 class Solution {
 public:
-    bool stoneGame(vector<int>& A) {
-        int n = A.size();
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
         if (~n & 1) return true;
 
         vector<int> dp(n);
 
         for (int i = n - 1; i >= 0; i--) {
-            dp[i] = A[i];
+            dp[i] = piles[i];
             for (int j = i + 1; j < n; j++)
-                dp[j] = max(A[i] - dp[j], A[j] - dp[j - 1]);
+                dp[j] = max(piles[i] - dp[j], piles[j] - dp[j - 1]);
         }
 
         return dp[n - 1] >= 0;
     }
 };
+
+
+
+// Solution - 1 : Recursion | Memoization
+
+// class Solution {
+// public:
+//     bool stoneGame(vector<int>& piles) {
+//         int n = piles.size();
+//         if (~n & 1) return true;
+
+//         vector dp(n, vector<int>(n, -1));
+//         auto maxDiff = [&](this auto&& maxDiff, int i, int j) -> int {
+//             int& res = dp[i][j];
+//             if (res != -1) return res;
+//             if (i == j) return res = piles[i];
+
+//             return res = max(piles[i] - maxDiff(i + 1, j),
+//                              piles[j] - maxDiff(i, j - 1));
+//         };
+
+//         return maxDiff(0, n - 1) >= 0;
+//     }
+// };
