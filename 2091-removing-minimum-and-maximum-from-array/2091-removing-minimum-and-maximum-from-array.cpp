51be241564f3fd9a0f07b8ffe50bd204621ac6c5
Omkar Sahasrabuddhe <143240322+OmkarS25@users.noreply.github.com>
2026-08-31 05:22:55 +0530

    Time: 0 ms (100%), Space: 92.2 MB (22.32%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        int n = nums.size();
+        const int n = nums.size();
+        int minIndex = 0, maxIndex = 0;
 
-        int minIndex = min_element(nums.begin(), nums.end()) - nums.begin();
-        int maxIndex = max_element(nums.begin(), nums.end()) - nums.begin();
+        // Find both the indexes to remove
+        for(int i=1; i<n; i++){
+            if(nums[minIndex] > nums[i]) minIndex = i;
+            if(nums[maxIndex] < nums[i]) maxIndex = i;
+        }
 
         int left = min(minIndex, maxIndex);
         int right = max(minIndex, maxIndex);
